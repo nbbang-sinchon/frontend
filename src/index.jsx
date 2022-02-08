6e9798b523d3f3fcfbd5ff23ba6125d2420ec464
korung3195 <vs970517@naver.com>
2022-02-08 18:33:59 +0900

    Style : eslint, prettier를 이용한 자동 포멧팅 적용
    - eslint, prettier 설정을 이용해 새로운 스타일을 적용하였습니다.
    - 컴포넌트는 모두 jsx 파일로 변경하였습니다.

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 
@@ -7,5 +8,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
