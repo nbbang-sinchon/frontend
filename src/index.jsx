71717bde70563eb5793b5b5400fe58b61f9f35f1
korung3195 <vs970517@naver.com>
2022-02-09 17:21:04 +0900

    Feat : reset global style
    global reset style을 적용하기 위해 Reset 컴포넌트를 만들고 적용시켰습니다.

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './App';
+import App from './components/App';
+import Reset from './styles/Reset';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Reset />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
---
+++ src/styles/Reset.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { css, Global } from '@emotion/react';
+
+const reset = css`
+  html,
+  body,
+  div,
+  span,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  img,
+  b,
+  i,
+  ol,
+  ul,
+  li,
+  form,
+  label,
+  legend,
+  caption,
+  canvas,
+  footer,
+  header,
+  nav,
+  section,
+  summary {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    outline: 0;
+    font-size: inherit;
+    color: inherit;
+    background: transparent;
+  }
+`;
+
+function Reset() {
+  return <Global styles={reset} />;
+}
+
+export default Reset;
