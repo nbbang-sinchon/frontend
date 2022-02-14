5c78a18899ccae1f62e154c44b8e9c92e7f1a757
korung3195 <vs970517@naver.com>
2022-02-14 19:02:25 +0900

    Feat : 헤더 컴포넌트 UI 구현
    헤더 컴포넌트의 UI를 구현하였습니다.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,92 @@
 import styled from '@emotion/styled';
 import React from 'react';
+import { icons } from '../assets/assets';
+import { COLORS, SIZES } from '../styles/constants';
+import Logo from './Logo';
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 10px;
+  border-bottom: 1px solid ${COLORS.PRIMARY};
+  min-width: ${SIZES.HEADER_MIN_WIDTH};
+`;
+
+const HeaderColumn = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+
+  &:nth-of-type(2n) {
+    flex: 2;
+  }
+
+  > svg {
+    margin: 0 5px;
+    min-width: 24px;
+
+    &:hover {
+      cursor: pointer;
+    }
+
+    @media only screen and (max-width: ${SIZES.HEADER_SMALL_WIDTH}) {
+      width: 24px;
+      height: 24px;
+    }
+  }
+`;
+
+const SearchBar = styled.div`
+  display: flex;
+
+  width: 100%;
+  padding: 5px 5px 5px 20px;
+  box-sizing: border-box;
+  border: 2px solid ${COLORS.PRIMARY};
+  border-radius: 30px;
+  color: ${COLORS.PRIMARY};
+
+  @media only screen and (max-width: ${SIZES.HEADER_SMALL_WIDTH}) {
+    font-size: 8px;
+    padding: 3px 3px 3px 10px;
+    border-radius: 25px;
+
+    > svg {
+      width: 24px;
+      height: 24px;
+    }
+  }
+`;
+
+const SearchBarInput = styled.input`
+  width: 100%;
+
+  &::placeholder {
+    color: ${COLORS.PRIMARY2};
+  }
+`;
 
 function Header() {
-  return <Container>This is header</Container>;
+  return (
+    <Container>
+      <HeaderColumn>
+        <Logo />
+      </HeaderColumn>
+      <HeaderColumn>
+        <SearchBar>
+          <SearchBarInput placeholder="카테고리명, 음식명 등을 검색하여 원하는 파티을 찾아 보세요!" />
+          <icons.SearchIcon />
+        </SearchBar>
+      </HeaderColumn>
+      <HeaderColumn>
+        <icons.PartyIcon />
+        <icons.ProfileIcon />
+        <icons.NotificationIcon />
+      </HeaderColumn>
+    </Container>
+  );
 }
 
 export default Header;
