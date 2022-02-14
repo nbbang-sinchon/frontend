4523e12e4043d3fad70536224a343370bd72ba4b
korung3195 <vs970517@naver.com>
2022-02-14 16:54:41 +0900

    Feat : 아이콘을 리액트 컴포넌트로 export하는 파일 추가
    svg를 하나씩 import하는 것은 효율적이지 않기 때문에 svg를 ReactComponent로 변환하여 export 하는 파일을 추가하였습니다.

---
+++ src/assets/icons.js
@@ -0,0 +1,6 @@
+import { ReactComponent as NotificationIcon } from './icons/notification.svg';
+import { ReactComponent as PartyIcon } from './icons/party.svg';
+import { ReactComponent as ProfileIcon } from './icons/profile.svg';
+import { ReactComponent as SearchIcon } from './icons/search.svg';
+
+export { NotificationIcon, PartyIcon, ProfileIcon, SearchIcon };
