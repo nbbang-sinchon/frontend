2c493936a137aa500a97395bc6d7f83471920f9a
joooonis <xowns3213@gmail.com>
2022-02-15 01:27:06 +0900

    Feat : Party 컴포넌트 구현

---
+++ src/components/Party.jsx
@@ -0,0 +1,99 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import { COLORS } from '../styles/constants';
+import { icons } from '../assets/assets';
+import PropTypes from "prop-types";
+
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+
+  flex-flow: column wrap;
+  padding: 10px;
+  flex: 1 1 2;
+`;
+
+const InnerContainer = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: start;
+`;
+
+const TagsContainer = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: start;
+  padding: 5px;
+`;
+
+
+const Title = styled.div`
+  
+  display: flex;
+  flex: auto;
+  padding: 20px;
+
+
+  font-color: ${COLORS.BLACK};
+  font-size: 30px;
+  font-weight: bolder;
+
+  background : ${COLORS.PRIMARY};
+  border-radius: 10px; 
+`;
+
+const Tag = styled.div`
+display: flex;
+margin: 2px;
+padding: 2px;
+align-items: flex-start;
+color: ${COLORS.PRIMARY};
+
+border: 1px solid ${COLORS.PRIMARY};
+border-radius: 2px;
+
+font-size: 10px;
+`;
+
+const Place = styled.div`
+  display: flex;
+  flex: 1;
+  color: ${COLORS.BLACKBLACK};
+
+  font-size: 1px;
+`;
+
+
+function Party({title, hashtags, place}) {
+    console.log(hashtags)
+    return (
+      <>
+      <Container>
+        <InnerContainer>
+          <Title>{title}</Title>
+        </InnerContainer>
+        <TagsContainer>
+          {hashtags.map(tag => (
+          <Tag key={tag}>#{tag}</Tag>
+          ))}
+        </TagsContainer>
+        <InnerContainer>
+        <Place><icons.LocationIcon/>{place}</Place>
+        <Place><icons.TimeIcon/>방금</Place>
+        </InnerContainer>
+        <InnerContainer>
+        </InnerContainer>
+      </Container>
+      </>
+    );
+  }
+
+ Party.propTypes = {
+    title: PropTypes.string.isRequired,
+    hashtags: PropTypes.arrayOf(PropTypes.string).isRequired,
+    place: PropTypes.string.isRequired,
+  };
+
+export default Party;
+  
