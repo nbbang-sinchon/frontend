2687f043488615140e2d10dc90e9a1aafd0f553e
korung3195 <vs970517@naver.com>
2022-02-14 18:32:42 +0900

    Feat : asset 들을 export하는 파일 추가
    기존 icon을 export 하던 파일에서 확장하여 asset들을 모두 export 하도록 변경하였습니다.

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,5 +2,9 @@ import { ReactComponent as NotificationIcon } from './icons/notification.svg';
 import { ReactComponent as PartyIcon } from './icons/party.svg';
 import { ReactComponent as ProfileIcon } from './icons/profile.svg';
 import { ReactComponent as SearchIcon } from './icons/search.svg';
+import logo from './images/logo.png';
 
-export { NotificationIcon, PartyIcon, ProfileIcon, SearchIcon };
+const icons = { NotificationIcon, PartyIcon, ProfileIcon, SearchIcon };
+const images = { logo };
+
+export { icons, images };
