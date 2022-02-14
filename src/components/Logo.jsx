cf86732af239a558010d6693aa66f69e3d11b1a8
korung3195 <vs970517@naver.com>
2022-02-14 19:01:54 +0900

    Feat : 로고에 width에 따른 반응성 추가
    로고가 일정 크기 미만으로 작아질 경우 따라서 작아지는 기능을 구현하였습니다.

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,21 +1,33 @@
 import styled from '@emotion/styled';
 import React from 'react';
 import { images } from '../assets/assets';
-import { COLORS } from '../styles/constants';
+import { COLORS, SIZES } from '../styles/constants';
 
 const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const Image = styled.img`
   height: 60px;
+
+  @media only screen and (max-width: ${SIZES.HEADER_SMALL_WIDTH}) {
+    height: 40px;
+  }
 `;
 
 const Title = styled.div`
   color: ${COLORS.PRIMARY};
   font-size: 36px;
+
+  @media only screen and (max-width: ${SIZES.HEADER_SMALL_WIDTH}) {
+    display: none;
+  }
 `;
 
 function Logo() {
