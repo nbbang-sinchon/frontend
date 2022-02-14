3bff706f02ba3b31c10bae3d208ae18a0e0da8a9
joooonis <xowns3213@gmail.com>
2022-02-15 01:28:15 +0900

    <Feat> : Main에 Parties 컴포넌트 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Header from '../components/Header';
+import Parties from '../components/Parties';
+
+
+
+
 
 function Main() {
+  
+
   return (
     <div>
       <Header></Header>
+      <Parties></Parties>
     </div>
   );
 }
