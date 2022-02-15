07af4cd5ca6e49c0941bb26698cabd9185073980
korung3195 <vs970517@naver.com>
2022-02-15 18:19:28 +0900

    Feat : 각 페이지 컴포넌트 추가
    채팅, 시작, 파티, 마이페이지로 사용될 컴포넌트들을 추가하고 라우팅을 설정하였습니다.

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 import Reset from './styles/Reset';
+import IndexPage from './pages/IndexPage';
+import MyPage from './pages/MyPage';
+import PartyPage from './pages/PartyPage';
+import ChattingPage from './pages/ChattingPage';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Reset />
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/party" element={<PartyPage />} />
+        <Route path="/chatting" element={<ChattingPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
