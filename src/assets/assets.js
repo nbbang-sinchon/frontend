ee6221d3d48e02c4db883c74a391a4ff5f52eb52
joooonis <xowns3213@gmail.com>
2022-02-15 18:18:02 +0900

    Chore : location 아이콘 파일 수정

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,9 +2,8 @@ import { ReactComponent as NotificationIcon } from './icons/notification.svg';
 import { ReactComponent as PartyIcon } from './icons/party.svg';
 import { ReactComponent as ProfileIcon } from './icons/profile.svg';
 import { ReactComponent as SearchIcon } from './icons/search.svg';
-import { ReactComponent as LocationIcon } from './icons/location_on.svg'
+import { ReactComponent as LocationIcon } from './icons/location.svg'
 import { ReactComponent as TimeIcon } from './icons/time.svg'
-
 import logo from './images/logo.png';
 
 const icons = { NotificationIcon, PartyIcon, ProfileIcon, SearchIcon, LocationIcon, TimeIcon };
