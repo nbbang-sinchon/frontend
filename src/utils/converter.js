2e035b16d005629020dacdbb64e6675d2beda830
korung3195 <vs970517@naver.com>
2022-02-16 16:01:54 +0900

    Feat : 단위 변환용 converter util 모듈 생성
    장소를 영어에서 한글로 변환시키고, date를 적절한 string으로 바꿔주는 util 함수를 생성하였습니다.

---
+++ src/utils/converter.js
@@ -0,0 +1,39 @@
+const convertPlace = (placeString) => {
+  const upper = placeString.toUpperCase();
+
+  if (upper === 'SINCHON') {
+    return '신촌동';
+  } else if (upper === 'CHANGCHEON') {
+    return '창천동';
+  } else if (upper === 'YEONHUI') {
+    return '연희동';
+  } else {
+    return 'ERROR';
+  }
+};
+
+const convertDate = (dateString) => {
+  const TIME_ZONE = 9;
+  const MILLI_TO_SECOND = 1000;
+  const SECOND_TO_MINUTE = 60;
+  const MINUTE_TO_HOUR = 60;
+  const HOUR_TO_DAY = 24;
+
+  const date = new Date(dateString);
+  date.setHours(date.getHours() - TIME_ZONE);
+
+  const now = new Date();
+  const diff = (now.getTime() - date.getTime()) / MILLI_TO_SECOND;
+
+  if (diff / SECOND_TO_MINUTE < 1) {
+    return '방금';
+  } else if (diff / SECOND_TO_MINUTE < MINUTE_TO_HOUR) {
+    return `${Math.floor(diff / SECOND_TO_MINUTE)}분 전`;
+  } else if (diff / SECOND_TO_MINUTE < MINUTE_TO_HOUR * HOUR_TO_DAY) {
+    return `${Math.floor(diff / (SECOND_TO_MINUTE * MINUTE_TO_HOUR))}시간 전`;
+  } else {
+    return `${Math.floor(diff / (SECOND_TO_MINUTE * MINUTE_TO_HOUR * HOUR_TO_DAY))}일 전`;
+  }
+};
+
+export { convertPlace, convertDate };
