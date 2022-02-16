4eda9a86d5933ca222e209e72c769db1398ab580
joooonis <xowns3213@gmail.com>
2022-02-17 02:58:56 +0900

    Feat : 메인페이지에 Footer 컴포넌트 추가
    생성한 Footer 컴포넌트를 메인페이지에 추가하였습니다.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Main from '../components/Main';
 import Parties from '../components/Parties';
@@ -10,6 +11,7 @@ function MainPage() {
       <Main>
         <Parties />
       </Main>
+      <Footer />
     </>
   );
 }
