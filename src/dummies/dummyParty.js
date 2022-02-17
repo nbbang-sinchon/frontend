dd0820c96fdca2dafe81f1ba560e5f40b5fbbb7b
korung3195 <vs970517@naver.com>
2022-02-17 17:33:23 +0900

    Chore : 파티 상세 더미데이터 생성
    파티 상세페이지에 사용할 더미데이터를 API 형식에 맞게 생성하였습니다.

--- src/dummies/dummyParty.js
+++ src/dummies/dummyParty.js
@@ -1,6 +1,6 @@
-const dummyParties = [
+const dummyParty = [
   {
-    partyId: 0,
+    ownerNickname: '재호',
     title: 'BHC 뿌링클 오늘 7시',
     createTime: '2022-02-14T05:25:17.410Z',
     joinNumber: 2,
@@ -8,9 +8,10 @@ const dummyParties = [
     status: 'order',
     place: 'SINCHON',
     hashtags: ['치킨', 'BHC', '배달비', '뿌링클'],
+    content: '오늘 7시에 BHC에서 배달 나눠 시키실 분 구합니다. 창서초 앞에서 픽업해요~',
   },
   {
-    partyId: 1,
+    ownerNickname: '현경',
     title: '내일 점심에 버거킹 드실분',
     createTime: '2022-02-15T14:25:17.410Z',
     joinNumber: 1,
@@ -18,9 +19,10 @@ const dummyParties = [
     status: 'soon',
     place: 'YEONHUI',
     hashtags: ['버거킹', '햄버거'],
+    content: '내일 버거킹 시켜서 서문 근처에서 받으실분 구합니다',
   },
   {
-    partyId: 2,
+    ownerNickname: '건호',
     title: '푸라닭 콘소메이징 맛있어요ㅛㅛㅛㅛㅛㅛ',
     createTime: '2022-02-15T15:25:17.410Z',
     joinNumber: 3,
@@ -28,9 +30,10 @@ const dummyParties = [
     status: 'full',
     place: 'CHANGCHEON',
     hashtags: ['치킨', '푸라닭'],
+    content: '푸라닭 개꿀맛',
   },
   {
-    partyId: 3,
+    ownerNickname: '태준',
     title: '초밥 시켜먹어요',
     createTime: '2022-02-15T18:25:17.410Z',
     joinNumber: 1,
@@ -38,9 +41,10 @@ const dummyParties = [
     status: 'cancel',
     place: 'SINCHON',
     hashtags: ['초밥', '스시'],
+    content: '학교에서 초밥 시켜서 나눠 받으실분!',
   },
   {
-    partyId: 4,
+    ownerNickname: '재호',
     title: '떡볶이 ㄱ?',
     createTime: '2022-02-16T15:59:17.410Z',
     joinNumber: 1,
@@ -48,7 +52,8 @@ const dummyParties = [
     status: 'on',
     place: 'CHANGCHEON',
     hashtags: ['떡볶이', '엽떡', '국대'],
+    content: '엽떡이나 국대 나눠 시키실 분 구합니다. 우체국 근처',
   },
 ];
 
-export default dummyParties;
+export default dummyParty;
