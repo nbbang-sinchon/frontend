b4c578433fe2b9130dc223de2e1584145fb6c49d
joooonis <xowns3213@gmail.com>
2022-02-17 21:36:51 +0900

    Feat : 마이페이지에 컴포넌트 추가
    MyPage에 MyPageUserInfo 컴포넌트를 추가하여 마이페이지를 완성했습니다.

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MyPageHeader from '../components/MyPageHeader';
+import MyPageUserInfo from '../components/MyPageUserInfo';
 
 function MyPage() {
   return (
     <>
       <MyPageHeader />
+      <MyPageUserInfo />
     </>
   );
 }
