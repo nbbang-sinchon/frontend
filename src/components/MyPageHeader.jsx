1997e98a1ef6e10bec709f7e8a13327f6e81406c
joooonis <xowns3213@gmail.com>
2022-02-18 00:22:57 +0900

    Chore : MyPageHeader 컴포넌트 수정
    컬럼을 사용하는 것으로 코드를 수정하였습니다. 그리고 내부 컴포넌트들의 위치를 조금 수정하였습니다.

--- src/components/MyPageHeader.jsx
+++ src/components/MyPageHeader.jsx
@@ -4,78 +4,87 @@ import { Link } from 'react-router-dom';
 import { images } from '../assets/assets';
 import { COLORS, SIZES } from '../styles/constants';
 
-function MyPageHeader() {
-  const Logo = () => {
-    const Container = styled.div`
-      display: flex;
-      &:hover {
-        cursor: pointer;
-      }
-    `;
-
-    const Image = styled.img`
-      height: 60px;
-
-      @media only screen and (max-width: ${SIZES.HEADER_MIDDLE_WIDTH}) {
-        height: 40px;
-      }
-    `;
-
-    return (
-      <Link to="/main">
-        <Container>
-          <Image src={images.logo} />
-        </Container>
-      </Link>
-    );
-  };
-
-  const Container = styled.header`
+const Logo = () => {
+  const Container = styled.div`
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-    padding: 10px;
-    border-bottom: 2px solid ${COLORS.PRIMARY};
-    min-width: ${SIZES.HEADER_MIN_WIDTH};
-    position: sticky;
-    top: 0;
-    background-color: white;
-    font-weight: 700;
-    z-index: 1;
-  `;
-
-  const WhiteSpace = styled.div`
-    width: 112.6px;
-    height: 50.33px;
-  `;
-
-  const LogOutButton = styled.div`
-    font-size: 20px;
-    font-weight: 400;
-    border: 1.5px solid;
-    border-radius: 15px;
-    color: ${COLORS.WHITE};
-    background-color: ${COLORS.PRIMARY};
-    padding: 9px 18px 9px 18px;
-
-    flex-grow: 0;
-    flex-shrink: 0;
-
     &:hover {
-      color: ${COLORS.WHITE};
-      background-color: ${COLORS.PRIMARY};
       cursor: pointer;
     }
   `;
 
+  const Image = styled.img`
+    height: 60px;
+
+    @media only screen and (max-width: ${SIZES.HEADER_MIDDLE_WIDTH}) {
+      height: 40px;
+    }
+  `;
+
+  return (
+    <Link to="/main">
+      <Container>
+        <Image src={images.logo} />
+      </Container>
+    </Link>
+  );
+};
+
+const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 10px;
+  border-bottom: 2px solid ${COLORS.PRIMARY};
+  min-width: ${SIZES.HEADER_MIN_WIDTH};
+  position: sticky;
+  top: 0;
+  background-color: white;
+  font-weight: 700;
+  z-index: 1;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+
+  &:nth-of-type(2) {
+    flex: 2;
+  }
+`;
+
+const LogOutButton = styled.div`
+  font-size: 20px;
+  font-weight: 400;
+  border: 1.5px solid;
+  border-radius: 15px;
+  color: ${COLORS.WHITE};
+  background-color: ${COLORS.PRIMARY};
+  padding: 9px 18px 9px 18px;
+
+  flex-grow: 0;
+  flex-shrink: 0;
+
+  &:hover {
+    color: ${COLORS.WHITE};
+    background-color: ${COLORS.PRIMARY};
+    cursor: pointer;
+  }
+`;
+
+function MyPageHeader() {
   return (
     <Container>
-      <WhiteSpace></WhiteSpace>
-      <Logo />
-      <Link to="/">
-        <LogOutButton>로그아웃</LogOutButton>
-      </Link>
+      <Column></Column>
+      <Column>
+        <Logo />
+      </Column>
+      <Column>
+        <Link to="/">
+          <LogOutButton>로그아웃</LogOutButton>
+        </Link>
+      </Column>
     </Container>
   );
 }
