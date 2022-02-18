7d167e28e86ee8a3c70a2d7c0d081ea30264e3b0
joooonis <xowns3213@gmail.com>
2022-02-18 19:18:13 +0900

    Chore : arrow 아이콘 추가
    시작페이지에서 사용할 arrow 아이콘을 추가하였습니다.

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,13 +5,14 @@ import { ReactComponent as SearchIcon } from './icons/search.svg';
 import { ReactComponent as LocationIcon } from './icons/location.svg';
 import { ReactComponent as TimeIcon } from './icons/time.svg';
 import { ReactComponent as HeartIcon } from './icons/heart.svg';
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
 import logo from './images/logo.png';
 import bread from './images/bread.png';
 import kakao from './images/Kakao.png';
 import naver from './images/Naver.png';
 import google from './images/Google.png';
 
-const icons = { NotificationIcon, PartyIcon, ProfileIcon, SearchIcon, LocationIcon, TimeIcon, HeartIcon };
+const icons = { NotificationIcon, PartyIcon, ProfileIcon, SearchIcon, LocationIcon, TimeIcon, HeartIcon, ArrowIcon };
 const images = { logo, bread, kakao, naver, google };
 
 export { icons, images };
