292026de7b2a06906269b48e7b4a5d6643cc3de9
joooonis <xowns3213@gmail.com>
2022-02-21 22:14:18 +0900

    Feat : 파티 생성 기능 구현
    완료 버튼을 누르면 확인메세지를 띄우고 확인이 되면 json객체를 생성하여 fetch url을 보냅니다. 받아온 partyID는 따로 저장합니다.
    cors 이슈로 아직 제대로 작동이 되는지 확인을 하지 못했습니다.

--- src/pages/NewPartyPage.jsx
+++ src/pages/NewPartyPage.jsx
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Main from '../components/Main';
+import { SWAGGER_URL } from '../config';
 import { NewPartyHeader, NewParty } from '../components/NewParty';
+import hashtagstringtolist from '../utils/hashtagstringtolist';
 
 function NewPartyPage() {
+  const [newPartyID, SetNewPartyID] = useState({ paryId: null });
+  const [newParty, SetNewParty] = useState({
+    title: '',
+    content: '',
+    hashtags: [],
+    place: 'SINCHON',
+    goalNumber: 0,
+  });
+
+  const onChange = (event) => {
+    const { value, name } = event.target;
+    SetNewParty({
+      ...newParty,
+      [name]: value,
+    });
+  };
+
+  const onClick = () => {
+    let message = confirm('파티를 생성하겠습니까?');
+
+    if (message) {
+      SetNewParty({
+        ...newParty,
+        ['hashtags']: hashtagstringtolist(newParty['hashtags']),
+      });
+
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newParty),
+      };
+
+      fetch(`${SWAGGER_URL}/parties`, requestOptions)
+        .then((response) => response.json())
+        .then((data) => SetNewPartyID({ paryId: data.id }));
+    } else {
+      console.log(newPartyID);
+      return;
+    }
+  };
+
   return (
     <>
-      <NewPartyHeader />
+      <NewPartyHeader onClick={onClick} />
       <Main>
-        <NewParty></NewParty>
+        <NewParty newparty={newParty} onChange={onChange} />
       </Main>
     </>
   );
