4de10972e4534d3e1a75701772ea32949ff0e727
korung3195 <vs970517@naver.com>
2022-02-22 17:56:43 +0900

    Feat : 채팅 스타일 변경
    카카오톡 느낌으로 닉네임이 위쪽에 표시되도록 해봤습니다.

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -12,57 +12,57 @@ const Container = styled.div`
 
   margin: 10px 0;
   width: 100%;
-
-  > div {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    &:last-of-type {
-      justify-content: flex-end;
-    }
-  }
 `;
 
-const Profile = styled.div`
-  font-size: 10px;
+const Image = styled.img`
+  width: 36px;
+  height: 36px;
+  object-fit: cover;
+  border-radius: 50%;
+  border: 1px solid ${COLORS.PRIMARY};
+  margin-right: 10px;
+`;
+
+const Nickname = styled.div`
+  font-size: 12px;
+  padding: 5px;
+  padding-top: 0;
 `;
 
 const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+
   max-width: 50%;
   font-size: 14px;
-  margin-left: 10px;
-  padding: 10px 15px;
+`;
+
+const InnerContent = styled.div`
+  height: fit-content;
   background: ${(props) => (props.isSender && COLORS.PRIMARY) || COLORS.GRAY};
+  padding: 5px 15px;
   color: ${(props) => props.isSender && COLORS.WHITE};
   border-radius: 20px;
 `;
 
 const Time = styled.div`
+  display: flex;
+  align-items: flex-end;
+
   font-size: 10px;
   color: ${COLORS.DARK_GRAY};
   white-space: nowrap;
-`;
-
-const Image = styled.img`
-  width: 24px;
-  height: 24px;
-  object-fit: cover;
-  border-radius: 50%;
-  border: 1px solid ${COLORS.PRIMARY};
+  margin: 0 5px;
 `;
 
 function Chat({ chat, isSender }) {
   return (
     <Container isSender={isSender}>
-      {!isSender && (
-        <Profile>
-          <Image src={images.logo} />
-          <div>{chat.sender.nickname}</div>
-        </Profile>
-      )}
-      <Content isSender={isSender}>{chat.content}</Content>
+      {!isSender && <Image src={images.logo} />}
+      <Content>
+        {!isSender && <Nickname>{chat.sender.nickname}</Nickname>}
+        <InnerContent isSender={isSender}>{chat.content}</InnerContent>
+      </Content>
       <Time>{convertDateToTime(chat.createTime)}</Time>
     </Container>
   );
