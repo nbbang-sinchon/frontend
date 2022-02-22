ac339585dcf09a3fa306cecf58a70821fdd4069c
joooonis <xowns3213@gmail.com>
2022-02-22 16:42:19 +0900

    Chore : 해시테그 관련 유틸 개선
    #는 메인에서 출력할때 붙여줌으로 유틸에서는 따로 붙여주지 않습니다.
    해시테그 input이 비어있는 채로 완료를 누르면 빈 리스트를 반환합니다.

--- src/utils/hashtagstringtolist.js
+++ src/utils/hashtagstringtolist.js
@@ -1,13 +1,8 @@
 const hashTagStringToList = (hashTagesString) => {
-  if (!hashTagesString) {
+  if (!hashTagesString || hashTagesString === null) {
     return [];
   } else {
-    let hashtags = hashTagesString
-      .replace(/(\s*)/g, '')
-      .split('#')
-      .map((s) => '#' + s);
-    hashtags.shift();
-    return hashtags;
+    return hashTagesString.split(' ');
   }
 };
 
