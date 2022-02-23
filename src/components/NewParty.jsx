c19fff617122164b0cbe19783e6732d2f5efc038
joooonis <xowns3213@gmail.com>
2022-02-23 18:03:04 +0900

    Feat : 파티 생성 구현
    파티를 생성할 수 있습니다.

--- src/components/NewParty.jsx
+++ src/components/NewParty.jsx
@@ -1,7 +1,87 @@
 import styled from '@emotion/styled';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
-import React from 'react';
-import { COLORS } from '../styles/constants';
+import { Link } from 'react-router-dom';
+import { icons, images } from '../assets/assets';
+import { COLORS, SIZES } from '../styles/constants';
+import { SWAGGER_URL } from '../config';
+import hashTagStringToList from '../utils/hashtagstringtolist';
+
+const Logo = () => {
+  const Container = styled.div`
+    display: flex;
+    &:hover {
+      cursor: pointer;
+    }
+  `;
+
+  const Image = styled.img`
+    height: 60px;
+
+    @media only screen and (max-width: ${SIZES.HEADER_MIDDLE_WIDTH}) {
+      height: 40px;
+    }
+  `;
+
+  return (
+    <Link to="/main">
+      <Container>
+        <Image src={images.logo} />
+      </Container>
+    </Link>
+  );
+};
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 10px;
+  border-bottom: 2px solid ${COLORS.PRIMARY};
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+
+  svg {
+    width: 35px;
+    height: 35px;
+  }
+
+  &:hover svg {
+    opacity: 0.8;
+    cursor: pointer;
+  }
+
+  &:nth-of-type(2) {
+    flex: 2;
+  }
+`;
+
+const SaveButton = styled.button`
+  font-size: 20px;
+  font-weight: 400;
+  border: 1.5px solid;
+  border-radius: 15px;
+  color: ${COLORS.WHITE};
+  background-color: ${COLORS.PRIMARY};
+  padding: 9px 18px 9px 18px;
+
+  flex-grow: 0;
+  flex-shrink: 0;
+
+  &:hover {
+    opacity: 0.8;
+    cursor: pointer;
+  }
+`;
 
 const Container = styled.div`
   height: 100vh;
@@ -87,43 +167,79 @@ const ContentInput = styled.textarea`
   color: ${COLORS.DARK_GRAY};
 `;
 
-function NewParty({ newparty, onChange }) {
+function NewParty() {
+  const formRef = useRef();
+
+  const createPartyObj = (newparty) => {
+    if (window.confirm('파티를 생성하시겠습니까?')) {
+      fetch(`${SWAGGER_URL}/parties`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newparty),
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response);
+        });
+    } else {
+      console.log('파티 만들기 취소');
+    }
+  };
+
+  const onClick = () => {
+    const form = new FormData(formRef.current);
+    const newParty = {
+      title: form.get('title'),
+      content: form.get('content'),
+      hashtags: hashTagStringToList(form.get('hashtags')),
+      place: form.get('place'),
+      goalNumber: form.get('goalNumber'),
+    };
+    createPartyObj(newParty);
+  };
+
   return (
-    <Container>
-      <InnerContainer>
-        <TitleInput name="title" placeholder="파티 제목을 입력하세요." onChange={onChange} value={newparty.title} />
-        <HashTagInput
-          name="hashtags"
-          placeholder="#해시태그로 파티 정보를 알려주세요."
-          onChange={onChange}
-          value={newparty.hashtags}></HashTagInput>
-        <Select>
-          <select name="place" onChange={onChange} value={newparty.place}>
-            <option value="SINCHON">신촌동</option>
-            <option value="YEONHUI">연희동</option>
-            <option value="CHANGCHEON">창천동</option>
-          </select>
-          <input
-            name="goalNumber"
-            type="number"
-            min="1"
-            placeholder="인원 수"
-            onChange={onChange}
-            value={newparty.goalNumber}></input>
-        </Select>
-        <ContentInput
-          name="content"
-          placeholder="내용을 입력하세요."
-          onChange={onChange}
-          value={newparty.content}></ContentInput>
-      </InnerContainer>
-    </Container>
+    <>
+      <Header>
+        <Column>
+          <Link to="/main">
+            <icons.CancelIcon />
+          </Link>
+        </Column>
+        <Column>
+          <Logo />
+        </Column>
+        <Column>
+          <SaveButton onClick={onClick}>완료</SaveButton>
+        </Column>
+      </Header>
+      <Container>
+        <InnerContainer>
+          <form ref={formRef}>
+            <TitleInput name="title" placeholder="파티 제목을 입력하세요." />
+            <HashTagInput name="hashtags" placeholder="#해시태그로 파티 정보를 알려주세요."></HashTagInput>
+            <Select>
+              <select name="place">
+                <option value="SINCHON">신촌동</option>
+                <option value="YEONHUI">연희동</option>
+                <option value="CHANGCHEON">창천동</option>
+              </select>
+              <input name="goalNumber" type="number" min="1" placeholder="인원 수"></input>
+            </Select>
+            <ContentInput name="content" placeholder="내용을 입력하세요."></ContentInput>
+          </form>
+        </InnerContainer>
+      </Container>
+    </>
   );
 }
 
-NewParty.propTypes = {
-  newparty: PropTypes.object.isRequired,
-  onChange: PropTypes.func.isRequired,
-};
-
 export default NewParty;
+
+NewParty.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
