5ef243490fc72f5f1316266d9d1121b1991f4264
joooonis <xowns3213@gmail.com>
2022-02-23 18:04:22 +0900

    Chore : 파티페이지 정리
    로직들은 전부 뉴파티컴포넌트안으로 이동하고 이동하고 나니 props도 따로 필요가 없어져서 정리를 했습니다.

--- src/pages/NewPartyPage.jsx
+++ src/pages/NewPartyPage.jsx
@@ -1,58 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Main from '../components/Main';
-import { SWAGGER_URL } from '../config';
-import NewPartyHeader from '../components/NeaPartyHeader';
 import NewParty from '../components/NewParty';
-import hashTagStringToList from '../utils/hashtagstringtolist';
 
 function NewPartyPage() {
-  // const [newPartyID, SetNewPartyID] = useState({ paryId: null });
-  const [newParty, SetNewParty] = useState({
-    title: '',
-    content: '',
-    hashtags: [],
-    place: 'SINCHON',
-    goalNumber: 0,
-  });
-
-  const onChange = (event) => {
-    const { value, name } = event.target;
-    SetNewParty({
-      ...newParty,
-      [name]: value,
-    });
-  };
-
-  const onClick = () => {
-    console.log(hashTagStringToList(newParty['hashtags']));
-
-    SetNewParty({
-      ...newParty,
-      ['hashtags']: hashTagStringToList(newParty['hashtags']),
-    });
-
-    if (window.confirm('파티를 생성하시겠습니까?')) {
-      fetch(`${SWAGGER_URL}/parties`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newParty),
-      })
-        .then((response) => response.json())
-        .then((response) => {
-          console.log(response);
-        });
-    } else {
-      console.log('파티 만들기 취소');
-    }
-  };
-
   return (
     <>
-      <NewPartyHeader onClick={onClick} />
       <Main>
-        <NewParty newparty={newParty} onChange={onChange} />
+        <NewParty />
       </Main>
     </>
   );
