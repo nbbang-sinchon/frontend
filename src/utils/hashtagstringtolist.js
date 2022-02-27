ca8c505b18a3dc911ba64d46c1baf38407098f42
joooonis <xowns3213@gmail.com>
2022-02-27 18:56:22 +0900

    Feat : 해시테그 관련 유틸 추가
    List를 String 으로 반환하는 유틸을 추가 하였습니다.
    또한 함수 실행 전 타입체크를 하도록 하였습니다.

--- src/utils/hashtagstringtolist.js
+++
@@ -1,9 +0,0 @@
-const hashTagStringToList = (hashTagesString) => {
-  if (!hashTagesString || hashTagesString === null) {
-    return [];
-  } else {
-    return hashTagesString.split(' ');
-  }
-};
-
-export default hashTagStringToList;
