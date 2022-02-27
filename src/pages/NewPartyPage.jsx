b39e0f0edff66ca91a6c6994d99e62d7ea27b27a
joooonis <xowns3213@gmail.com>
2022-02-27 19:05:26 +0900

    Feat : 뉴파티페이지에 파티수정 페이지 추가
    url이 newparty/id 인 경우 id, party를 props로 전달합니다. 두개의 props가 전달된 경우 뉴파티페이지는 파티수정페이지입니다.

--- src/pages/NewPartyPage.jsx
+++ src/pages/NewPartyPage.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import Main from '../components/Main';
 import NewParty from '../components/NewParty';
+import useParty from '../hooks/useParty';
 
 function NewPartyPage() {
+  const { id } = useParams();
+  const { party } = useParty(id);
+
   return (
     <>
-      <Main background="WHITE">
-        <NewParty />
-      </Main>
+      <Main background="WHITE">{id ? <NewParty id={id} party={party} /> : <NewParty />}</Main>
     </>
   );
 }
