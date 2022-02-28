67e5852a542e5655932cb35ba04a4974f609bdf1
joooonis <xowns3213@gmail.com>
2022-02-28 18:57:42 +0900

    Chore : 상수 추가
    백엔드에서 만들어 준 구글로그인에 필요한 url을 임시로 추가했습니다.

--- src/config.js
+++ src/config.js
@@ -2,4 +2,7 @@ const SERVER_URL = 'http://15.165.132.250:8094';
 const CHAT_PAGE_SIZE = '20';
 const PARTY_PAGE_SIZE = '10';
 
-export { SERVER_URL, CHAT_PAGE_SIZE, PARTY_PAGE_SIZE };
+const GOOGLE_LOGIN_URL =
+  'http://ec2-15-165-132-250.ap-northeast-2.compute.amazonaws.com:8094/oauth2/authorization/google';
+
+export { SERVER_URL, CHAT_PAGE_SIZE, PARTY_PAGE_SIZE, GOOGLE_LOGIN_URL };
