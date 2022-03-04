7f084abaf9c766718b49e1e47cff9f403a8e01b6
joooonis <xowns3213@gmail.com>
2022-03-04 17:35:56 +0900

    Feat : 나의파티페이지 구현

--- src/pages/MyPartyPage.jsx
+++ src/pages/MyPartyPage.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
-import Main from '../components/Main';
+import Main2 from '../components/Main2';
+import MyParties from '../components/Myparties';
+import MyPartyHeader from '../components/MyPartyHeader';
+import useMyParty from '../hooks/useMyParty';
 
 function MyPartyPage() {
+  const [isOnGoing, setIsOnGoing] = useState(true);
+  const { parties } = useMyParty(isOnGoing);
+
+  const toggleParties = () => setIsOnGoing(!isOnGoing);
+
   return (
     <>
       <Header />
-      <Main></Main>
+      <Main2>
+        {isOnGoing ? (
+          <>
+            <MyPartyHeader header={'참여 중인 파티 목록'} onClick={toggleParties} />
+            <MyParties parties={parties} />
+          </>
+        ) : (
+          <>
+            <MyPartyHeader header={'종료된 파티'} onClick={toggleParties} />
+            <MyParties parties={parties} />
+          </>
+        )}
+      </Main2>
     </>
   );
 }
