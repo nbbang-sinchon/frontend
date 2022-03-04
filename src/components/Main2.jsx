6654d87abbe73bc21303737d180f01773ebe5ede
joooonis <xowns3213@gmail.com>
2022-03-04 17:26:28 +0900

    Feat : Main2 컴포넌트 생성
    나의파티페이지에서 사용할 Main을 만들었습니다. 기존의 메인과는 padding값만 다릅니다.

---
+++ src/components/Main2.jsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { COLORS, SIZES } from '../styles/constants';
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background-color: ${(props) => COLORS[props.background] || COLORS.PRIMARY};
+  min-width: ${SIZES.MIN_WIDTH};
+`;
+
+const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  position: relative;
+  width: 100%;
+  max-width: ${SIZES.MAIN_MAX_WIDTH};
+  box-sizing: border-box;
+  background-color: white;
+
+  height: ${(props) => props.fitHeight && `calc(100vh - ${SIZES.HEADER_HEIGHT_LARGE})`};
+  @media only screen and (max-width: ${SIZES.MIDDLE_WIDTH}) {
+    height: ${(props) => props.fitHeight && `calc(100vh - ${SIZES.HEADER_HEIGHT_MIDDLE})`};
+  }
+  @media only screen and (max-width: ${SIZES.SMALL_WIDTH}) {
+    height: ${(props) => props.fitHeight && `calc(100vh - ${SIZES.HEADER_HEIGHT_SMALL})`};
+  }
+`;
+
+function Main2({ children, background, fitHeight }) {
+  return (
+    <Container background={background?.toUpperCase()}>
+      <InnerContainer fitHeight={fitHeight}>{children}</InnerContainer>
+    </Container>
+  );
+}
+
+Main2.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+  background: PropTypes.string,
+  fitHeight: PropTypes.bool,
+};
+
+export default Main2;
