d3fc11c95c4c47c8d64f28b18a5ef50467186813
korung3195 <vs970517@naver.com>
2022-03-07 19:29:54 +0900

    Feat : 채팅 메뉴 UI 구성
    채팅 메뉴에 사용할 간단한 UI를 구성했습니다.

--- src/components/Chat/ChatMenu.jsx
+++ src/components/Chat/ChatMenu.jsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { images } from '../../assets/assets';
+import plainButton from '../../styles/plainButton';
 
 const Container = styled.div`
   position: absolute;
@@ -9,8 +10,6 @@ const Container = styled.div`
   right: ${(props) => (props.isShown && '-40px') || '-500px'};
   transition: right ease-in-out 0.3s;
   width: 500px;
-  background-color: white;
-  z-index: 1;
 `;
 
 const Image = styled.img`
@@ -18,10 +17,30 @@ const Image = styled.img`
   width: 100%;
 `;
 
+const Content = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+
+  width: 280px;
+  margin: 150px 0 0 80px;
+`;
+
+const Menu = styled(plainButton)`
+  margin-bottom: 25px;
+`;
+
 function ChatMenu({ isShown }) {
   return (
     <Container isShown={isShown}>
       <Image src={images.breadBoard} />
+      <Content>
+        <Menu>파티 나가기</Menu>
+        <Menu>모집 상태 변경</Menu>
+        <Menu>파티 수정</Menu>
+        <Menu>파티 보기</Menu>
+        <Menu>파티 삭제</Menu>
+      </Content>
     </Container>
   );
 }
